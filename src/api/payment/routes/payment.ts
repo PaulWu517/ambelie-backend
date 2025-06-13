bb930440ac90a96e620233fdcf54a1ef9c233ecd
PaulWu517 <pwu709724@gmail.com>
2025-06-13 10:05:57 +0800

    Add files via upload

---
+++ src/api/payment/routes/payment.ts
@@ -0,0 +1,30 @@
+export default {
+  routes: [
+    // 默认CRUD路由
+    {
+      method: 'GET',
+      path: '/payments',
+      handler: 'payment.find',
+    },
+    {
+      method: 'GET',
+      path: '/payments/:id',
+      handler: 'payment.findOne',
+    },
+    {
+      method: 'POST',
+      path: '/payments',
+      handler: 'payment.create',
+    },
+    {
+      method: 'PUT',
+      path: '/payments/:id',
+      handler: 'payment.update',
+    },
+    {
+      method: 'DELETE',
+      path: '/payments/:id',
+      handler: 'payment.delete',
+    },
+  ],
+}; 
