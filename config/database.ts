3c5315461f10e28028bda7b770e82ec9af85e7d2
PaulWu517 <pwu709724@gmail.com>
2025-07-02 09:16:30 +0800

    Add files via upload

--- config/database.ts
+++ config/database.ts
@@ -11,15 +11,15 @@ export default ({ env }) => {
         client: 'postgres',
         connection: {
           connectionString: env('DATABASE_URL'),
-        ssl: env.bool('DATABASE_SSL', false) && {
+          ssl: env.bool('DATABASE_SSL', false) && {
             rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
+          },
         },
-      },
         pool: {
           min: env.int('DATABASE_POOL_MIN', 2),
-          max: env.int('DATABASE_POOL_MAX', 10),
-    },
-        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 60000),
+          max: env.int('DATABASE_POOL_MAX', 5),
+        },
+        acquireConnectionTimeout: env.int('DATABASE_CONNECTION_TIMEOUT', 30000),
         debug: false,
       },
     };
