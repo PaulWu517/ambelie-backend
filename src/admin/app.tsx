467acc49f0f352147eb60111b08e6c5de7061937
PaulWu517 <pwu709724@gmail.com>
2025-07-09 09:00:58 +0800

    Add files via upload

---
+++ src/admin/app.tsx
@@ -0,0 +1,40 @@
+import type { StrapiApp } from '@strapi/strapi/admin';
+
+export default {
+  config: {
+    locales: [
+      // 'ar',
+      // 'fr',
+      // 'cs',
+      // 'de',
+      // 'dk',
+      // 'es',
+      // 'he',
+      // 'id',
+      // 'it',
+      // 'ja',
+      // 'ko',
+      // 'ms',
+      // 'nl',
+      // 'no',
+      // 'pl',
+      // 'pt-BR',
+      // 'pt',
+      // 'ru',
+      // 'sk',
+      // 'sv',
+      // 'th',
+      // 'tr',
+      // 'uk',
+      // 'vi',
+      // 'zh-Hans',
+      // 'zh',
+    ],
+  },
+  register(app: StrapiApp) {
+    // This register function ensures CKEditor presets are available
+  },
+  bootstrap(app: StrapiApp) {
+    console.log('Bootstrap complete', app);
+  },
+}; 
